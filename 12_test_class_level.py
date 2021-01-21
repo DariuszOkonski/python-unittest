568a22deee1a017a90a6bd4ca00fc00b87626975
Dariusz Okonski <darek200180@gmail.com>
2021-01-21 22:48:11 +0000

    add module 12_test_class_level

---
+++ 12_test_class_level.py
@@ -0,0 +1,30 @@
+import unittest
+
+def setUpModule():
+    print('setting up module...')
+
+def tearDownModule():
+    print('tearing down module...')
+
+class TestClass1(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        print('setting up TestClass1')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print('trearing down TestClass1')
+
+    def test_case_1(self):
+        self.assertEqual('John Smith'.split(), ['John', 'Smith'])
+
+class TestClass2(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('3.0'.split('.'), ['3', '0'])
+
+class TestClass3(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('#'.join(['sport', 'gym']), 'sport#gym')
