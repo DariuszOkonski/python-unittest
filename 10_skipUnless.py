547c49941cc0386e79634a10a045503af2abac47
Dariusz Okonski <darek200180@gmail.com>
2021-01-21 22:31:53 +0000

    add module 10_skipUnless

---
+++ 10_skipUnless.py
@@ -0,0 +1,14 @@
+import sys
+import unittest
+
+class TestClass(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual('aws'.upper(), 'AWS')
+
+    @unittest.skipUnless(sys.platform.startswith('linus'), 'Requires linux')
+    def test_case_2(self):
+        self.assertEqual('aws'.upper(), 'AWS')
+
+    @unittest.skipUnless(sys.platform.startswith('win'), 'Requires windows')
+    def test_case_3(self):
+        self.assertEqual('aws'.upper(), 'AWS')
