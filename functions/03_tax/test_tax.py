25dde16a05f2a1e31e976f22d8af6cd1b4e9eb52
Dariusz Okonski <darek200180@gmail.com>
2021-01-25 20:58:17 +0000

    add files tax and test_tax

---
+++ functions/03_tax/test_tax.py
@@ -0,0 +1,12 @@
+import unittest
+from tax import calc_tax
+
+class TestTax(unittest.TestCase):
+    def test_calc_tax_width_ten_percent(self):
+        self.assertEqual(10, calc_tax(100, 10))
+
+    def test_calc_tax_width_fourteen_percent(self):
+        self.assertEqual(14, calc_tax(100, 14))
+
+    def test_calc_tax_width_fourteen_percent_with_almost_equal(self):
+        self.assertAlmostEqual(14, calc_tax(100, 14))
